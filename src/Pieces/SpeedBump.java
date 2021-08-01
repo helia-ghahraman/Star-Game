85ec876411bdf2f7303117641334b87ad58cfc14
Helia Ghahraman <76126890+helia-ghahraman@users.noreply.github.com>
2021-08-01 17:23:07 +0430

    Delete src directory

--- src/Pieces/SpeedBump.java
+++
@@ -1,9 +0,0 @@
-package Pieces;
-
-import javax.swing.*;
-
-public class SpeedBump {
-
-    public static final ImageIcon speedbump_icon = new ImageIcon(new ImageIcon("src/Pictures/speed_bump.png")
-            .getImage().getScaledInstance(70, 70, 70));
-}
--- src/Pieces/Star.java
+++
@@ -1,9 +0,0 @@
-package Pieces;
-
-import javax.swing.*;
-
-public class Star {
-
-    public static final ImageIcon star_icon = new ImageIcon(new ImageIcon("src/Pictures/star.png")
-            .getImage().getScaledInstance(70, 70, 70));
-}
